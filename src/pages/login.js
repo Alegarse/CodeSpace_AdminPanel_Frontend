686f9d03dd3db90f143a69d341ea8c1c02216ef4
tatianamarchi <tani16145@gmail.com>
2025-05-09 18:07:20 +0200

    create login-container

---
+++ src/pages/login.js
@@ -0,0 +1,20 @@
+function createLoginPage() {
+
+    const loginElement = document.createElement('div');
+    loginElement.classList = 'login-container';
+
+    const loginTitle = document.createElement('h2');
+    loginTitle.textContent = 'Iniciar sesion';
+
+    const formContainer = document.createElement('form');
+    formContainer.classList = 'form-login';
+    formContainer.id = 'form-login';
+
+    const labelEmail = document.createElement('label');
+    labelEmail.textContent = 'Usuario';
+
+    
+
+
+}
+
