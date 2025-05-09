6bacca7e1788bc335503466ea18bd23ce80606b1
Alejandro García Serrano <agscm2@gmail.com>
2025-05-09 17:23:51 +0200

    Pasos a realizar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+
+/**
+ * PASOS NECESARIOS 
+ * 1. Crear los inputs y sus correspondientes label
+ * 2. Crear el boton para hacer login
+ * 3. Imagen representativa
+ * 4. Link para recuperar contraseña
+ */
