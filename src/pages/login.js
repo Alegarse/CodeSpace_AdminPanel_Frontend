c0ac61f844de9e419bd3ece78c483447f0c8c441
elena <elenaobregonl@gmail.com>
2025-05-09 18:33:35 +0200

    create Input-Label

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,37 @@
 function createLoginPage() {
+  const loginElement = document.createElement("div");
+  loginElement.classList = "login-container";
 
-    const loginElement = document.createElement('div');
-    loginElement.classList = 'login-container';
+  const loginTitle = document.createElement("h2");
+  loginTitle.textContent = "Iniciar sesion";
 
-    const loginTitle = document.createElement('h2');
-    loginTitle.textContent = 'Iniciar sesion';
+  const formContainer = document.createElement("form");
+  formContainer.classList = "form-login";
+  formContainer.id = "form-login";
 
-    const formContainer = document.createElement('form');
-    formContainer.classList = 'form-login';
-    formContainer.id = 'form-login';
+  const labelEmail = document.createElement("label");
+  labelEmail.textContent = "Usuario";
+  labelEmail.htmlFor = "input-email";
 
-    const labelEmail = document.createElement('label');
-    labelEmail.textContent = 'Usuario';
+  const inputEmail = document.createElement("input");
+  inputEmail.setAttribute("required", true);
+  inputEmail.placeholder = "example@dominio.com";
+  inputEmail.id = "input-email"
+  inputEmail.type = "text";
 
-    
+  const labelPassword = document.createElement("label");
+  labelPassword.textContent = "Contraseña";
+  labelPassword.htmlFor = "input-password";
 
+  const inputPassword = document.createElement("input");
+  inputPassword.setAttribute("required", true);
+  inputPassword.placeholder = "********";
+  inputPassword.id = "input-password"
+  inputPassword.type = "password";
 
+  const buttonLogin = document.createElement("button");
+  buttonLogin.type = "submit";
+  buttonLogin.classList = "button-login";
+  buttonLogin.textContent = "Entrar";
+  
 }
-
