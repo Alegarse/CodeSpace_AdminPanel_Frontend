45149fc90e8e8baa89bdc3eb63d19cdc6d52c37d
Alejandro García Serrano <agscm2@gmail.com>
2025-05-16 22:57:22 +0200

    Fix

--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,6 @@ export async function loginPanel(userEmail, userPassword) {
         let loginUrl = 'http://localhost:3000/api/login';
         loginUrl += `?userEmail=${userEmail}`;
         loginUrl += `?userPassword=${userPassword}`;
-        console.log(loginUrl);
         return (await axios(loginUrl))?.data;
     } catch (error) {
         console.error(error.message);
